7298152bc6e27c84c3e0e19e6da1f44ce8c78a12
XGHeaven <xgheaven@gmail.com>
2016-06-07 11:41:10 +0800

    update version

--- src/main/java/cn/xgheaven/usefulsolar/Main.java
+++ src/main/java/cn/xgheaven/usefulsolar/Main.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class Main
 {
     public static final String MODID = "usefulsolar";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.2.0";
 
     // proxy
     @SidedProxy(clientSide = "cn.xgheaven.usefulsolar.common.proxy.ClientProxy", serverSide = "cn.xgheaven.usefulsolar.common.proxy.ServerProxy")
