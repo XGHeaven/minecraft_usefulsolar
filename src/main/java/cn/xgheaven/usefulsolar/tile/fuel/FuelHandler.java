15a22bdbace5479b0bfbf6995a9775320b1f319c
XGHeaven <xgheaven@gmail.com>
2016-06-12 12:22:04 +0800

    add lava_coal_block to fuelList

--- src/main/java/cn/xgheaven/usefulsolar/tile/fuel/FuelHandler.java
+++ src/main/java/cn/xgheaven/usefulsolar/tile/fuel/FuelHandler.java
@@ -3,19 +3,18 @@ package cn.xgheaven.usefulsolar.tile.fuel;
 import cn.xgheaven.usefulsolar.tile.USBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.IFuelHandler;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import sun.awt.FullScreenCapable;
 
 /**
  * Created by xgheaven on 6/7/16.
  */
 public class FuelHandler implements IFuelHandler {
 
-    public static Block[] fuelList = {
-            USBlocks.lava_block
+    private static Block[] fuelList = {
+            USBlocks.lava_block,
+            USBlocks.lava_coal_block
     };
 
     public static void registerFuelHandler() {
